cc4a164af9343847c69911a1587852bd0d6e4e32
Justin Hamerly <suvimountain@icloud.com>
2023-03-10 17:33:55 -0800

    topics slice can add new topics.  the selector function and action creator are being exported

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,15 +1,29 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-
+  topics: [],
+  createTopicIsPending: false,
+  failedToCreateTopic: false,
 }
 
 export const topicsSlice = createSlice({
   name: 'topics',
   initialState,
-  extraReducers: (builder) => {
-
+  reducers: {
+    addTopic: (state, action) => {
+      state.topics.push(action.payload)
+    }
   }
 })
 
+
+//selector functions exports
+export const getTopics = (state) => state.topics;
+
+//actions object export
+export const {
+  addTopic
+} = topicsSlice.actions;
+
+//reducer export
 export default topicsSlice.reducer;
