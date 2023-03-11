553c0bba6842b4104611d430092b5cec53402bf0
Justin Hamerly <suvimountain@icloud.com>
2023-03-10 17:23:18 -0800

    reducer file initialized for topics

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,15 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+}
+
+export const topicsSlice = createSlice({
+  name: 'topics',
+  initialState,
+  extraReducers: (builder) => {
+
+  }
+})
+
+export default topicsSlice.reducer;
